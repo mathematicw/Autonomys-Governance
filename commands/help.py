b6bb012631993c28909a1741bf514ebfba29361c
Douceur <florian.goudot@gmail.com>
2025-07-21 10:30:51 +0200

    fix:  add vote to helper

--- commands/help.py
+++ commands/help.py
@@ -5,5 +5,6 @@ def helper_wrapper(client):
     async def help_command(interaction: discord.Interaction):
         embed = discord.Embed(title="Autonomys Governance Assistant", description="Available commands:", color=0x3498db)
         embed.add_field(name="/proposal", value="Proposal menu", inline=False)
+        embed.add_field(name="/vote", value="Vote menu", inline=False)
         embed.set_footer(text="See you later fellow ambassador")
         await interaction.response.send_message(embed=embed)
